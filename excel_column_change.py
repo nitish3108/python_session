d1a1d53718729d14c1f6c9ec9c15b3adebe012ee
nitish3108 <snitish134@gmail.com>
2023-08-23 09:49:01 +0530

    Updated as per expected input csv file

--- excel_column_change.py
+++ excel_column_change.py
@@ -1,14 +1,26 @@
 
-input_file = '/mnt/d/linux/python_session/input_csv.csv'  
+input_file = '/mnt/d/linux/python_session/machine-readable-business-employment-data-mar-2023-quarter.csv'  
 output_file = '/mnt/d/linux/python_session/output_csv.csv'  
+
+data_value_column_index = 2
+subject_column_index = 7
 
 def run():
 	ifh = open(input_file)
 	ofh = open(output_file, "w")
-	for line in ifh:
+	for index, line in enumerate(ifh):
+		output_string = ""
 		line = line.strip()
-		a,b,c,d,e,f = line.split(",")
-		output_string = a + "," + d + "," +  c  + "," + b + "," +  e + "," + f + "\n"
-		ofh.write(output_string)
+		input_line_list = line.split(",")
+		#print(columns)
+		input_line_list[data_value_column_index], input_line_list[subject_column_index] = \
+								input_line_list[subject_column_index],\
+								input_line_list[data_value_column_index]
 
-assert run() == None
+		line = ','.join(input_line_list) + '\n'
+		ofh.write(line)
+	ifh.close()
+	ofh.close()
+	print("Completed")
+
+#assert run() == None
